0c0a6b4ae24c2ebb0646ac2860a22d960d827954
jgcar27 <gre8one7@hotmail.com>
2022-09-05 16:55:09 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -183,7 +183,7 @@ export const managers = [
        "tookOver": 2022, // (optional) used if a manager took over a team, delete this line or change to null otherwise
        "location": "Illinois", // (optional)
        "bio": "Lorem ipsum...",
-       "photo": "/managers/trey.jpg", // square ratio recommended (no larger than 500x500)
+       "photo": "/managers/Trey.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
        "favoriteTeam": "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
    //    "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
